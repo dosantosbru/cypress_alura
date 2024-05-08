27c2e82a66cc32704da2e953bc5cd604e5a7063d
Bruno Oliveira <dosantosbru@icloud.com>
2024-05-08 00:12:19 -0300

    Novo cenário incluído

--- cypress/e2e/cadastro-incorreto,cy.js
+++ cypress/e2e/cadastro-incorreto,cy.js
@@ -1,11 +1,12 @@
 describe("Página de cadastro", () => {
-  it("Deve preencher os campos do formulário corretamente para cadastrar um novo usuário", () => {
-    cy.visit("https://adopet-frontend-cypress.vercel.app/");
+  it("Preencher os campos do formulário incorretamente e exibir mensagens ao usuário", () => {
+    cy.visit("https://adopet-frontend-iota.vercel.app/");
     cy.get('[data-test="register-button"]').click();
-    cy.get('[data-test="input-name"]').type("Bruno Oliveira");
-    cy.get('[data-test="input-email"]').type("dosantosbru@icloud.com");
-    cy.get('[data-test="input-password"]').type("Senha123");
-    cy.get('[data-test="input-confirm-password"]').type("Senha123");
     cy.get('[data-test="submit-button"]').click();
+    cy.contains("É necessário informar um endereço de email").should(
+      "be.visible"
+    );
+    cy.contains("Crie uma senha").should("be.visible");
+    cy.contains("Repita a senha criada acima").should("be.visible");
   });
 });
